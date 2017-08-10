3e4bb6340c532975ade9aaa9980f6f0b1a92b859
Philipp Korber <philippkorber@gmail.com>
2017-08-10 13:42:32 +0200

    chore(mail/Resource): added empty_text constructor
    Makes creating tests simpler

--- src/mail/resource.rs
+++ src/mail/resource.rs
@@ -6,6 +6,7 @@ use futures::future::BoxFuture;
 use error::Error;
 
 use super::mime::SinglepartMime;
+use mime::TEXT_PLAIN;
 use types::buffer::FileBuffer;
 
 
@@ -19,6 +20,14 @@ pub enum Resource {
     },
     FileBuffer( FileBuffer ),
     Future( BoxFuture<FileBuffer, Error> )
+}
+
+impl Resource {
+
+    /// creates a new Ressource with content type "text/plain" and an empty body
+    pub fn empty_text() -> Self {
+        Resource::FileBuffer( FileBuffer::new( TEXT_PLAIN, Vec::new() ) )
+    }
 }
 
 impl fmt::Debug for Resource {
@@ -38,6 +47,23 @@ impl fmt::Debug for Resource {
             Future( .. ) => {
                 write!( fter, "Future( .. )")
             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn empty_text_resource() {
+        let er = Resource::empty_text();
+        match er {
+            Resource::FileBuffer( fb ) => {
+                assert_eq!( &TEXT_PLAIN, fb.content_type() );
+                assert_eq!( "".as_bytes(), &*fb );
+            },
+            other => panic!( "unexpected kind of empty text: {:?}", other )
         }
     }
 }
