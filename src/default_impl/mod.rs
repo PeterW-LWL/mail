8c3bf47e6232278ab49b5c5f8b730523b5122d6d
Philipp Korber <philippkorber@gmail.com>
2017-08-14 14:01:57 +0200

    chore(default_impl): more fine grained feature controll

--- src/default_impl/mod.rs
+++ src/default_impl/mod.rs
@@ -1,18 +1,18 @@
 
 
 
-#[cfg(feature="default_impl_mail")]
+#[cfg(feature="default_impl_cpupool")]
 mod cpupool;
-#[cfg(feature="default_impl_mail")]
+#[cfg(feature="default_impl_cpupool")]
 pub use self::cpupool::*;
 
 
-#[cfg(feature="default_impl_mail")]
+#[cfg(feature="default_impl_fs")]
 pub mod fs;
-#[cfg(feature="default_impl_mail")]
+#[cfg(feature="default_impl_fs")]
 pub use self::fs::*;
 
-#[cfg(feature="default_impl_mail")]
+#[cfg(feature="default_impl_vfs")]
 pub mod vfs;
-#[cfg(feature="default_impl_mail")]
+#[cfg(feature="default_impl_vfs")]
 pub use self::vfs::*;
