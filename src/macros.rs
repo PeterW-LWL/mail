5e1dfb122d4dc2fba6b02683ca4e546e1235846d
Philipp Korber <philippkorber@gmail.com>
2017-08-29 18:33:14 +0200

    chore(test): added assert_err! macro and opt context for assert_ok/err

--- src/macros.rs
+++ src/macros.rs
@@ -8,6 +8,29 @@ macro_rules! assert_ok {
             Err( err ) => panic!( "expected Ok(..) got Err({:?})", err)
         }
     });
+    ($val:expr, $ctx:expr) => ({
+        match $val {
+            Ok( res ) => res,
+            Err( err ) => panic!( "expected Ok(..) got Err({:?}) [ctx: {:?}]", err, $ctx)
+        }
+    });
+}
+
+#[cfg(test)]
+#[macro_export]
+macro_rules! assert_err {
+    ($val:expr) => ({
+        match $val {
+            Ok( val ) => panic!( "expected Err(..) got Ok({:?})", val),
+            Err( err ) => err,
+        }
+    });
+    ($val:expr, $ctx:expr) => ({
+        match $val {
+            Ok( val ) => panic!( "expected Err(..) got Ok({:?}) [ctx: {:?}]", val, $ctx),
+            Err( err ) => err,
+        }
+    });
 }
 
 #[macro_export]
