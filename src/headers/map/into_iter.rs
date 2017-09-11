40c3c3b64a23332e450cd36356fb099a08411254
Philipp Korber <philippkorber@gmail.com>
2017-09-11 13:28:12 +0200

    chore(headers/map): into_iter now also respects ordering

--- src/headers/map/into_iter.rs
+++ src/headers/map/into_iter.rs
@@ -1,11 +1,8 @@
-use std::{self, mem};
-use std::collections::hash_map::{ IntoIter as MapIntoIter };
+use std::{mem, vec};
 
 use codec::{ MailEncoder, MailEncodable };
 use headers::HeaderName;
 
-
-use super::HeaderBodies;
 use super::HeaderMap;
 
 impl<E> IntoIterator for HeaderMap<E>
@@ -16,28 +13,27 @@ impl<E> IntoIterator for HeaderMap<E>
 
     fn into_iter(self) -> Self::IntoIter {
         let HeaderMap { header_map, header_vec } = self;
-        mem::drop(header_vec);
+        //TODO HeaderMap should use a ptr's for the header_map part
+        //1. drop header_map without droping any of the data backing it
+        for (_, bodies) in header_map.into_iter() {
+            //we still have the handle in header_vec
+            mem::forget(bodies.first);
+            if let Some( other ) = bodies.other {
+                for component_box in other.into_iter() {
+                    mem::forget(component_box);
+                }
+            }
+        }
         IntoIter {
-            header_map_iter: header_map.into_iter(),
-            sub_iter: None
+            header_vec_iter: header_vec.into_iter()
         }
     }
 }
 
-type _Iter<E> = std::iter::FlatMap<
-    std::option::IntoIter<
-        std::vec::Vec<Box<MailEncodable<E> + 'static>>
-    >,
-    std::vec::IntoIter<
-        Box<MailEncodable<E> + 'static>
-    >,
-    fn(Vec<Box<MailEncodable<E>>>) -> std::vec::IntoIter<Box<MailEncodable<E>>>
->;
 
 
 pub struct IntoIter<E: MailEncoder> {
-    header_map_iter: MapIntoIter<HeaderName, HeaderBodies<E>>,
-    sub_iter: Option<(HeaderName, _Iter<E>)>
+    header_vec_iter: vec::IntoIter<(HeaderName, *mut MailEncodable<E>)>
 }
 
 impl<E> Iterator for IntoIter<E>
@@ -46,32 +42,56 @@ impl<E> Iterator for IntoIter<E>
     type Item = (HeaderName, Box<MailEncodable<E>>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let result = self.sub_iter.as_mut().and_then( |&mut (ref name, ref mut iter)| {
-            iter.next().map( |hbody| (*name, hbody) )
-        } );
-        if result.is_some() {
-            return result;
-        } else {
-            let hbodies = self.header_map_iter.next();
-            if let Some( ( name, HeaderBodies { first, other } ) ) = hbodies {
-                self.sub_iter = Some( (
-                    name,
-                    other
-                        .into_iter()
-                        .flat_map( |el| el
-                            .into_iter() )
-                ) );
-                return Some( (name, first) )
-            } else {
-                return None;
-            }
-        }
+        self.header_vec_iter.next()
+            .map(|(name, ptr)| {
+                //SAFE: this *mut ptr is 1. unique and owining
+                //  we can only get here by destructing a HeaderMap
+                //  with into_iter, in which case we already removed
+                //  the ptr's in header_map
+                (name, unsafe { Box::from_raw(ptr) })
+            })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let (min, _max) = self.header_map_iter.size_hint();
-        // we have at last as many elements as the header map has entries,
-        // but might have more
-        (min, None)
+        self.header_vec_iter.size_hint()
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use codec::{ MailEncodable, MailEncoderImpl};
+    use headers::{ To, Subject, From};
+    use components::Unstructured;
+    use super::HeaderMap;
+    use super::super::downcast_ref;
+
+    #[test]
+    fn into_iter() {
+        const TEST_TEXT: &str = "this is a subject";
+        let mut headers = HeaderMap::<MailEncoderImpl>::new();
+        headers.insert(To, [ "affen@haus" ]).unwrap();
+        headers.insert(Subject, TEST_TEXT).unwrap();
+        headers.insert(From, [ "nix@da", "ja@wohl" ]).unwrap();
+
+        let headers = headers.into_iter()
+            .map(|(name, val)| {
+                (name.as_str(), val)
+            })
+            .collect::<Vec<_>>();
+        assert_eq!(3, headers.len());
+        //check order
+        assert_eq!("To", headers[0].0 );
+        assert_eq!("Subject", headers[1].0);
+        assert_eq!("From", headers[2].0 );
+
+        //check if we can use the data
+        let obj: &MailEncodable<_> = &*headers[1].1;
+        let text = downcast_ref::<_, Unstructured>(obj).unwrap();
+        assert_eq!(
+            TEST_TEXT,
+            text.as_str()
+        );
+
     }
 }
