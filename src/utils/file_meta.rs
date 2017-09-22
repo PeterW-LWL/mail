e67ba8c46d62fb75611dd1c1c52f6c04a9beb475
Philipp Korber <philippkorber@gmail.com>
2017-09-22 14:29:02 +0200

    chore(components/disposition): now supports arbitrary utf8 file names
    closes issue: #14
    ref issue: #11

--- src/utils/file_meta.rs
+++ src/utils/file_meta.rs
@@ -1,16 +1,10 @@
 
-use ascii::AsciiString;
 
 use super::DateTime;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Default)]
-pub struct  FileMeta {
-    // in rust std this is OsString, but we can not have it
-    // os specific in any way, as it is send over internet,
-    // originally this was Ascii, but has been extended
-    // to support encoding
-    // FEATURE_TODO(utf8_file_names): AsciiString => String
-    pub file_name: Option<AsciiString>,
+pub struct FileMeta {
+    pub file_name: Option<String>,
     pub creation_date: Option<DateTime>,
     pub modification_date: Option<DateTime>,
     pub read_date: Option<DateTime>,
