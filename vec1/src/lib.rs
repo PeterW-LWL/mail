9a66b3c79a2b00153f4def5ff850f5a035107caf
Philipp Korber <philippkorber@gmail.com>
2017-10-20 14:39:54 +0200

    test(vec1): implemented test making sure most vec methoes are aviable

--- vec1/src/lib.rs
+++ vec1/src/lib.rs
@@ -8,15 +8,20 @@ use std::iter::IntoIterator;
 
 #[macro_export]
 macro_rules! vec1 {
-    ( $first:expr, $($item:expr),* ) => (
-        #[allow(unused_mut)]
-        {
-            let mut tmp = $crate::Vec1::new( $first );
-            $( tmp.push( $item ); )*
-            tmp
-        }
+    ( $first:expr) => (
+         $crate::Vec1::new( $first )
     );
+    ( $first:expr,) => (
+         $crate::Vec1::new( $first )
+    );
+    ( $first:expr, $($item:expr),* ) => ({
+        let mut tmp = $crate::Vec1::new( $first );
+        $( tmp.push( $item ); )*
+        tmp
+    });
 }
+
+
 
 #[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
 pub struct Size0Error;
@@ -34,7 +39,7 @@ impl StdError for Size0Error {
 
 type Vec1Result<T> = StdResult<T, Size0Error>;
 
-#[derive( Debug, Clone, Eq, PartialEq, Hash )]
+#[derive( Debug, Clone, Eq,  Hash )]
 pub struct Vec1<T>(Vec<T>);
 
 impl<T> IntoIterator for Vec1<T> {
@@ -86,18 +91,6 @@ impl<T> Vec1<T> {
         self.0.last_mut().unwrap()
     }
 
-    pub fn reserve(&mut self, additional: usize) {
-        self.0.reserve( additional )
-    }
-
-    pub fn reserve_exact(&mut self, additional: usize) {
-        self.0.reserve_exact( additional )
-    }
-
-    pub fn shrink_to_fit(&mut self) {
-        self.0.shrink_to_fit()
-    }
-
     pub fn try_truncate(&mut self, len: usize) -> Vec1Result<()> {
         if len > 0 {
             self.0.truncate( len );
@@ -107,20 +100,12 @@ impl<T> Vec1<T> {
         }
     }
 
-    pub fn as_mut_slice(&mut self) -> &mut [T] {
-        self.0.as_mut_slice()
-    }
-
     pub fn try_swap_remove(&mut self, index: usize) -> Vec1Result<T> {
         if self.len() > 1 {
-            Ok( self.swap_remove( index ) )
+            Ok( self.0.swap_remove( index ) )
         } else {
             Err( Size0Error )
         }
-    }
-
-    pub fn insert(&mut self, index: usize, element: T) {
-        self.0.insert( index, element )
     }
 
     pub fn try_remove( &mut self, index: usize ) -> Vec1Result<T> {
@@ -128,6 +113,17 @@ impl<T> Vec1<T> {
             Ok( self.0.remove( index ) )
         } else {
             Err( Size0Error )
+        }
+    }
+
+    pub fn try_split_off(&mut self, at: usize) -> Vec1Result<Vec1<T>> {
+        if at == 0 {
+            Err(Size0Error)
+        } else if at >= self.len() {
+            Err(Size0Error)
+        } else {
+            let out = self.0.split_off(at);
+            Ok(Vec1(out))
         }
     }
 
@@ -144,9 +140,6 @@ impl<T> Vec1<T> {
         self.0.dedup_by( same_bucket )
     }
 
-    pub fn push(&mut self, value: T) {
-        self.0.push( value )
-    }
 
     /// pops if there is _more_ than 1 element in the vector
     pub fn pop(&mut self) -> Option<T> {
@@ -157,10 +150,38 @@ impl<T> Vec1<T> {
         }
     }
 
-    pub fn append(&mut self, other: &mut Vec<T>) {
-        self.0.append( other )
+    pub fn as_vec(&self) -> &Vec<T> {
+        &self.0
     }
 
+}
+
+macro_rules! impl_wrapper {
+    (pub $T:ident>
+        $(fn $name:ident(&$($m:ident)* $(, $param:ident: $tp:ty)*) -> $rt:ty);*) => (
+            impl<$T> Vec1<$T> {$(
+                #[inline]
+                pub fn $name(self: impl_wrapper!{__PRIV_SELF &$($m)*} $(, $param: $tp)*) -> $rt {
+                    (self.0).$name($($param),*)
+                }
+            )*}
+    );
+    (__PRIV_SELF &mut self) => (&mut Self);
+    (__PRIV_SELF &self) => (&Self);
+}
+
+impl_wrapper! {
+    pub T>
+        fn reserve(&mut self, additional: usize) -> ();
+        fn reserve_exact(&mut self, additional: usize) -> ();
+        fn shrink_to_fit(&mut self) -> ();
+        fn as_mut_slice(&mut self) -> &mut [T];
+        fn push(&mut self, value: T) -> ();
+        fn append(&mut self, other: &mut Vec<T>) -> ();
+        fn insert(&mut self, idx: usize, val: T) -> ();
+        fn len(&self) -> usize;
+        fn capacity(&self) -> usize;
+        fn as_slice(&self) -> &[T]
 }
 
 
@@ -178,6 +199,12 @@ impl<T> Vec1<T> where T: Clone {
     }
 }
 
+impl<T> Vec1<T> where T: PartialEq<T> {
+    pub fn dedub(&mut self) {
+        self.0.dedup()
+    }
+}
+
 
 impl<T> Vec1<T> where T: PartialEq<T> {
     pub fn dedup(&mut self) {
@@ -187,7 +214,7 @@ impl<T> Vec1<T> where T: PartialEq<T> {
 
 
 impl<T> Deref for Vec1<T> {
-    type Target = Vec<T>;
+    type Target = [T];
 
     fn deref( &self ) -> &Self::Target {
         &self.0
@@ -205,4 +232,184 @@ impl<T> Into<Vec<T>> for Vec1<T> {
     fn into( self ) -> Vec<T> {
         self.0
     }
+}
+
+impl<A, B> PartialEq<Vec1<B>> for Vec1<A>
+    where A: PartialEq<B>
+{
+    fn eq(&self, other: &Vec1<B>) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl<A, B> PartialEq<B> for Vec1<A>
+    where Vec<A>: PartialEq<B>
+{
+    fn eq(&self, other: &B) -> bool {
+        self.0.eq(other)
+    }
+}
+
+
+
+
+#[cfg(test)]
+mod test {
+
+    #[macro_export]
+    macro_rules! assert_ok {
+        ($val:expr) => ({
+            match $val {
+                Ok( res ) => res,
+                Err( err ) => panic!( "expected Ok(..) got Err({:?})", err)
+            }
+        });
+        ($val:expr, $ctx:expr) => ({
+            match $val {
+                Ok( res ) => res,
+                Err( err ) => panic!( "expected Ok(..) got Err({:?}) [ctx: {:?}]", err, $ctx)
+            }
+        });
+    }
+
+    macro_rules! assert_err {
+        ($val:expr) => ({
+            match $val {
+                Ok( val ) => panic!( "expected Err(..) got Ok({:?})", val),
+                Err( err ) => err,
+            }
+        });
+        ($val:expr, $ctx:expr) => ({
+            match $val {
+                Ok( val ) => panic!( "expected Err(..) got Ok({:?}) [ctx: {:?}]", val, $ctx),
+                Err( err ) => err,
+            }
+        });
+    }
+
+    mod Size0Error {
+        #![allow(non_snake_case)]
+        use super::super::*;
+
+        #[test]
+        fn implements_std_error() {
+            fn comp_check<T: StdError>(){}
+            comp_check::<Size0Error>();
+        }
+    }
+
+    mod Vec1 {
+        #![allow(non_snake_case)]
+        use super::super::*;
+
+        #[test]
+        fn now_warning_on_empty_vec() {
+            #![deny(warnings)]
+
+            let _ = vec1![1u8,];
+            let _ = vec1![1u8];
+
+        }
+
+        #[test]
+        fn deref_slice() {
+            let vec = Vec1::new(1u8);
+            let _: &[u8] = &*vec;
+        }
+
+        #[test]
+        fn deref_slice_mut() {
+            let mut vec = Vec1::new(1u8);
+            let _: &mut [u8] = &mut *vec;
+        }
+
+        #[test]
+        fn provided_all_ro_functions() {
+            let vec = Vec1::new(1u8);
+            assert_eq!(vec.len(), 1);
+            assert!(vec.capacity() > 0);
+            assert_eq!(vec.as_slice(), &*vec);
+            // there is obviously no reason we should provide this,
+            // as it can't be empty at all, that's the point behind Vec1
+            //assert_eq!(vec.is_empty(), true)
+        }
+
+        #[test]
+        fn provides_some_safe_mut_functions() {
+            let mut vec = Vec1::new(1u8);
+            vec.reserve(12);
+            assert!(vec.capacity() >= 13);
+            vec.reserve_exact(31);
+            assert!(vec.capacity() >= 31);
+            vec.shrink_to_fit();
+            let _: &mut [u8] = vec.as_mut_slice();
+            vec.insert(1, 31u8);
+            vec.insert(1, 2u8);
+            assert_eq!(&*vec, &[1, 2, 31]);
+            vec.dedup_by_key(|k| *k/3);
+            assert_eq!(&*vec, &[1, 31]);
+            vec.push(31);
+            assert_eq!(&*vec, &[1, 31, 31]);
+            vec.dedup_by(|l,r| l == r);
+            assert_eq!(&*vec, &[1, 31]);
+            vec.extend_from_slice(&[31,2,3]);
+            assert_eq!(&*vec, &[1, 31, 31, 2, 3]);
+            vec.dedub();
+            assert_eq!(&*vec, &[1, 31, 2, 3]);
+            // as the passed in vec is emptied this won't work with a vec1 as parameter
+            vec.append(&mut vec![1,2,3]);
+            assert_eq!(&*vec, &[1, 31, 2, 3, 1, 2, 3])
+        }
+
+        #[test]
+        fn provides_other_methos_in_failible_form() {
+            let mut vec = vec1![1u8,2,3,4];
+            assert_ok!(vec.try_truncate(3));
+            assert_err!(vec.try_truncate(0));
+            assert_eq!(vec, &[1,2,3]);
+
+            assert_ok!(vec.try_swap_remove(0));
+            assert_eq!(vec, &[3, 2]);
+            assert_ok!(vec.try_remove(0));
+            assert_eq!(vec, &[2]);
+            assert_err!(vec.try_swap_remove(0));
+            assert_err!(vec.try_remove(0));
+            vec.push(12);
+
+            assert_eq!(vec.pop(), Some(12));
+            assert_eq!(vec.pop(), None);
+            assert_eq!(vec, &[2]);
+
+        }
+
+        #[test]
+        fn try_split_of() {
+            let mut vec = vec1![1,2,3,4];
+            assert_err!(vec.try_split_off(0));
+            let len = vec.len();
+            assert_err!(vec.try_split_off(len));
+            let nvec = assert_ok!(vec.try_split_off(len-1));
+            assert_eq!(vec, &[1,2,3]);
+            assert_eq!(nvec, &[4]);
+        }
+
+        #[test]
+        fn try_resize() {
+            let mut vec = Vec1::new(1u8);
+            assert_ok!(vec.try_resize(10,2u8));
+            assert_eq!(vec.len(), 10);
+            assert_ok!(vec.try_resize(1, 2u8));
+            assert_eq!(vec, &[1]);
+            assert_err!(vec.try_resize(0, 2u8));
+        }
+
+
+        #[test]
+        fn with_capacity() {
+            let vec = Vec1::with_capacity(1u8, 16);
+            assert!(vec.capacity() >= 16);
+        }
+    }
+
+
 }
